231c682b5fe4ada123323d0dac3d013461e8eebb
Manoj Karthick <manojkarthick@ymail.com>
2020-12-31 01:54:08 -0800

    Add helper to check for string contains

--- utils/helpers.go
+++ utils/helpers.go
@@ -30,3 +30,12 @@ func ReadCSVFile(fileName string) ([][]string, error) {
 
 	return records, nil
 }
+
+func Contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
